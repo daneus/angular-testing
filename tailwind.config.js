4c50ca3a9d6030339ce3ae1361dd5a16ce4034b8
Ezkrman <tony.almonacid@hotmail.com>
2023-02-16 17:43:00 -0500

    Search bar component done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        azul: "#6366F0",
+        rosa: "#FF4181",
+        verdeBG: "#F3F5EE",
+        rojoBG: "#FFECEC",
+        morado: "#6D28DC",
+      },
+    },
   },
   plugins: [],
 };
