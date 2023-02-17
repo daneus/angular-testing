31cdb8668f67ee232d36538e9183ce563ac37616
Ezkrman <tony.almonacid@hotmail.com>
2023-02-17 15:31:36 -0500

    Search bar component moved into it's own component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BarraBusquedaComponent } from './componentes/barra-busqueda/barra-busqueda.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, BarraBusquedaComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/componentes/barra-busqueda/barra-busqueda.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-barra-busqueda',
+  templateUrl: './barra-busqueda.component.html',
+  styleUrls: ['./barra-busqueda.component.css']
+})
+export class BarraBusquedaComponent {
+
+}
